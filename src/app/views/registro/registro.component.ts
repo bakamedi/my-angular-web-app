f35be0d743c4e2087c61e2048f0ea13a2ecc2262
Bakke Medina <bakamedi12@gmail.com>
2018-02-21 00:48:58 -0500

    error solucionado

--- src/app/views/registro/registro.component.ts
+++ src/app/views/registro/registro.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators, FormBuilder, ReactiveFormsModule} from '@angular/forms';
 import { UserModel } from '../../../../src/app/models/user.model';
 
 @Component({
@@ -9,32 +8,9 @@ import { UserModel } from '../../../../src/app/models/user.model';
 })
 export class RegistroComponent implements OnInit {
 
-
-  modeloUsuarioRegistro: UserModel; // modelo de usuario
-  //Formulario de Registrar Usuario
-  userRegistroForm = new FormGroup({
-    'CORREO': new FormControl('', Validators.required),
-    'PASSWORD': new FormControl('', Validators.required),
-    'NOMBRE': new FormControl('', Validators.required),
-    'APELLIDO': new FormControl('', Validators.required),
-  });
-
-  constructor( private formBuilder: FormBuilder) { }
+  constructor( ) { }
 
   ngOnInit() {
-  }
-
-  formularioRegistro(){
-    this.userRegistroForm = this.formBuilder.group({
-      'CORREO': this.modeloUsuarioRegistro.CORREO,
-      'PASSWORD': this.modeloUsuarioRegistro.PASSWORD,
-      'NOMBRE': new FormControl('', Validators.required),
-      'APELLIDO': new FormControl('', Validators.required),
-    });
-  }
-
-  registrarUser = function(user){
-    console.log(user);
   }
 
 }
